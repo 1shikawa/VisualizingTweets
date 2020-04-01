75d8a8688ee567fb00adbf9a031167b4db791138
1shikawa <ishikawa.toru@gmail.com>
2020-04-01 11:51:49 +0900

    views update

--- VisualizingTweets/views.py
+++ VisualizingTweets/views.py
@@ -82,7 +82,8 @@ class Index(TemplateView):
                             se = pd.Series([
                                 tweet.id,
                                 tweet.created_at,
-                                tweet.text.replace('\n', ''),
+                                # tweet.text.replace('\n', ''),
+                                tweet.text
                                 tweet.favorite_count,
                                 tweet.retweet_count,
                                 URL+user_id +'/status/'+tweet.id_str #ツイートリンクURL
