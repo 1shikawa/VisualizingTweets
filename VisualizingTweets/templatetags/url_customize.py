f822815ffed5b6f5fbe52e8d9b4912f6eedea8dd
1shikawa <ishikawa.toru@gmail.com>
2020-04-01 13:15:59 +0900

    templatetag add

---
+++ VisualizingTweets/templatetags/url_customize.py
@@ -0,0 +1,7 @@
+from django import template
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+def url_target_blank(text):
+    return text.replace('<a ', '<a target="_blank" rel="nofollow" ')
